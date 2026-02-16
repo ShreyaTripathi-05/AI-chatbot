86d9081c72e2da5b93f93ba20dfac39df164a224
shreya <shreyatripathi9211@gmail.com>
2026-02-16 23:40:17 +0530

    changed index.js

--- index.js
+++ index.js
@@ -32,4 +32,9 @@ app.post("/chat", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running...");
+});
+
